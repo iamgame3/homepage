6c960e3c9ca2c06b09831c8125ef7f34bc8df1c2
iamgame3 <kosmosworld9@hotmail.com>
2024-01-02 13:48:28 -0500

    Add background color for header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
+import "./normalize.css";
 import "./index.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <div className="header-bg-color" />
+    </div>
+  );
 }
 
 export default App;
